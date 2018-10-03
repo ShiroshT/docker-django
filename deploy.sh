2470ac42cb5c663fba82aa143dcebb3f4e08975d
VitokitoDev <shiroshana@gmail.com>
2018-10-03 17:03:24 +0100

    first commit - travis file added

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+docker build -t ickyatcity/django-complex:latest -t ickyatcity/django-complex:$SHA -f ./complex/Dockerfile ./complex
+
+docker push ickyatcity/django-complex:latest
+
+docker push ickyatcity/django-complex:$SHA 
+
---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/c084217/development/django_projects/env/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
