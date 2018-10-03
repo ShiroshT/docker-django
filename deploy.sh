0c29c829ad2c311f6f1303d7eb5870dc9be663d2
VitokitoDev <shiroshana@gmail.com>
2018-10-03 17:42:43 +0100

    first commit - travis file updated -v1

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-docker build -t ickyatcity/django-complex:latest -t ickyatcity/django-complex:$SHA -f ./complex/Dockerfile ./complex
+docker build -t ickyatcity/django-complex:latest ./complex/Dockerfile ./complex
 
 docker push ickyatcity/django-complex:latest
 
